0b8bb12dc4945bb4ce5c0bdc5a7f107bdcece749
saniya.bhaldar96@gmail.com <saniya.bhaldar96@gmail.com>
2022-10-13 04:45:08 +0530

    Added scenarios for chat functionality

---
+++ test/specs/example.e2e.ts
@@ -0,0 +1,27 @@
+import LoginPage from  '../pageobjects/login.page';
+import SecurePage from '../pageobjects/secure.page';
+import AccTestData from '../testdata/acc.testdata';
+import ChatPage from '../pageobjects/chat.page';
+
+
+describe('My Login application', () => {
+    xit('should login and logout with valid credentials', async () => {
+        await LoginPage.open();
+        await LoginPage.login(AccTestData.TC1.OrganizationID,AccTestData.TC1.Username,AccTestData.TC1.Password);
+        await expect(SecurePage.accUser).toBeExisting();
+        await SecurePage.logout();
+        expect(SecurePage.lableLogin).toHaveTextContaining(AccTestData.TC2.expectLoginLable);       
+    });
+
+    it('should login with valid credentials and initiate a chat', async () => {
+        await LoginPage.open();
+        await LoginPage.login(AccTestData.TC1.OrganizationID,AccTestData.TC1.Username,AccTestData.TC1.Password);
+        await expect(SecurePage.accUser).toBeExisting();
+        await ChatPage.openChatUrl();
+        await ChatPage.initiateChat();
+        //browser.pause(5000);
+        await expect(await ChatPage.getAccNmChat).toBeDisplayed();
+    });
+});
+
+
